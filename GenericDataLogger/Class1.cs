21f56237f99aaf39af12af02723ec1fc04b01f4d
Andrew Yasman <ajyasman@gmail.com>
2019-12-04 01:58:09 -0700

    Additional testing stuff.

--- GenericDataLogger/Class1.cs
+++ GenericDataLogger/Class1.cs
@@ -17,19 +17,23 @@ namespace AYLib.GenericDataLogger
     /// Protobuf Data
     /// JSON binary data
     /// </summary>
-    public class Class1
+    public class Class1 : IDisposable
     {
+        private WriteDataBuffer writeBuffer = new WriteDataBuffer();
+
         private Guid Signature = Guid.Parse("46429DF1-46C8-4C0D-8479-A3BCB6A87643");
-        
+
+        private Header headerData = new Header();
+
         public Class1()
         {
-            Signature.ToByteArray();
+            //Signature.ToByteArray();
 
-            Header headerData = new Header();
+            
 
-            headerData.TypeRegistrations.Add(0, new TypeRegistration(0, typeof(Class1)));
-            headerData.TypeRegistrations.Add(1, new TypeRegistration(1, typeof(Class1)));
-            headerData.TypeRegistrations.Add(2, new TypeRegistration(2, typeof(Class1)));
+            //headerData.TypeRegistrations.Add(0, new TypeRegistration(0, typeof(Class1)));
+            //headerData.TypeRegistrations.Add(1, new TypeRegistration(1, typeof(Class1)));
+            //headerData.TypeRegistrations.Add(2, new TypeRegistration(2, typeof(Class1)));
 
             //var data = MessagePackSerializer.Serialize(typeList);
             //var back = MessagePackSerializer.Deserialize<Dictionary<string, string>>(data);
@@ -40,20 +44,20 @@ namespace AYLib.GenericDataLogger
             try
             {
                 //TypeRegistration newReg = new TypeRegistration(0, typeof(Class1));
-                var data = MessagePackSerializer.Serialize(headerData);
-                var back = MessagePackSerializer.Deserialize<Header>(data);
+                //var data = MessagePackSerializer.Serialize(headerData);
+                //var back = MessagePackSerializer.Deserialize<Header>(data);
 
-                //using (var fileStream = new FileStream("", FileMode.Append, FileAccess.Write, FileShare.None))
+                //using (var fileStream = new FileStream(@"TestFile.dat", FileMode.Append, FileAccess.Write, FileShare.None))
                 //{
-                //    WriteDataBlock(fileStream);
+                //    CreateHeader(fileStream);
                 //}
 
-                using (var memStream = new MemoryStream())
-                {
-                    WriteDataBlock(memStream);
-                }
+                //using (var memStream = new MemoryStream())
+                //{
+                //    CreateHeader(memStream);
+                //}
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
 
             }
@@ -61,16 +65,88 @@ namespace AYLib.GenericDataLogger
 
         public void RegisterType(Type newType)
         {
-
+            headerData.RegisterType(newType);
         }
 
-        public void WriteDataBlock(Stream targetStream)
+        public void CreateHeader(Stream targetStream)
         {
-            using (var bWriter = new BinaryWriter(targetStream))
+            WriteDataBlock(targetStream, Signature.ToByteArray(), true);
+            WriteDataBlock(targetStream, MessagePackSerializer.Serialize(headerData), false);
+        }
+
+        public void WriteDataBlock(Stream targetStream, byte[] data, bool isSignature = false)
+        {
+            using (var bWriter = new BinaryWriter(targetStream, System.Text.Encoding.Default, true))
             {
-                bWriter.Write(543543534);
+                if (!isSignature)
+                    bWriter.Write(data.Length);
+                bWriter.Write(data);
             }
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                    writeBuffer.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+
+    public class WriteDataBuffer : IDisposable
+    {
+        private MemoryStream memoryStream;
+        private BinaryWriter binaryWriter;
+
+        public WriteDataBuffer()
+        {
+            memoryStream = new MemoryStream();
+            binaryWriter = new BinaryWriter(memoryStream, System.Text.Encoding.Default, true);
+        }
+
+        public void WriteDataBlock(byte[] data, bool isSignature = false)
+        {
+            if (!isSignature)
+                binaryWriter.Write(data.Length);
+            binaryWriter.Write(data);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                    binaryWriter.Dispose();
+                    memoryStream.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
     }
 
     [MessagePackObject]
@@ -82,6 +158,12 @@ namespace AYLib.GenericDataLogger
         public Header()
         {
             TypeRegistrations = new Dictionary<int, TypeRegistration>();
+        }
+
+        public void RegisterType(Type newType)
+        {
+            int id = TypeRegistrations.Count;
+            TypeRegistrations.Add(id, new TypeRegistration(id, newType));
         }
     }
 
